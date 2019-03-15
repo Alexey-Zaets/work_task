068b1013b2a22582b16f02166537498a7aba375f
Alexey Zaets <3aets.dev@gmail.com>
2019-03-15 13:28:07 +0200

    fixed bug

--- test-task/app/webblog/blog/views.py
+++ test-task/app/webblog/blog/views.py
@@ -150,7 +150,8 @@ class ReplyCommentView(CreateView):
         if comment.level < 2:
             if form.is_valid():
                 new_comment = form.save(commit=False)
-                new_comment.author, new_comment.post = author, post
+                new_comment.author = author if isinstance(author, User) else None
+                new_comment.post = post
                 new_comment.level = comment.level + 1
                 new_comment.save()
                 comment.comments.add(new_comment)
