80cea75da620a13633a16ba116b2ee2c7425d1ae
Alexey Zaets <3aets.dev@gmail.com>
2019-03-27 16:42:35 +0200

    update navbar

--- test-task/frontend/src/components/Navbarcollapse.jsx
+++ test-task/frontend/src/components/Navbarcollapse.jsx
@@ -4,35 +4,50 @@ import Cookies from 'universal-cookie'
 
 
 class Navbarcollapse extends Component {
+    state = {
+        resetToken: true
+    }
 
     handleOnClickSignOut = (e) => {
         e.preventDefault();
         let cookies = new Cookies()
         cookies.remove('token')
+        this.setState({resetToken: !this.state.resetToken})
     }
 
     render() {
-        return (
-            <div className="collapse navbar-collapse">
-                <ul className="navbar-nav">
-                    <li className="nav-item active">
-                        <Link className="nav-link" to="/api/v1/post">Blog</Link>
-                    </li>
-                    <li className="nav-item active">
-                        <Link className="nav-link" to="/api/v1/post">+ Add new post</Link>
-                    </li>
-                </ul>
-                <ul className="navbar-nav ml-auto">
-                    <li className="nav-item active">
-                        <Link className="nav-link" to="/api/v1/user/login">Sign in</Link>
-                    </li>
-                    <li className="nav-item active">
-                        <Link className="nav-link" to="/api/v1/user/register">Sign up</Link>
-                    </li>
-                </ul>
-                <button className="btn btn-primary" onClick={this.handleOnClickSignOut}>Sign out</button>
-            </div>
-        )
+        const cookies = new Cookies()
+        const token = cookies.get('token')
+
+        if (token) {
+            return (
+                <div className="collapse navbar-collapse">
+                    <ul className="navbar-nav">
+                        <li className="nav-item active">
+                            <Link className="nav-link" to="/api/v1/post">Blog</Link>
+                        </li>
+                        <li className="nav-item active">
+                            <Link className="nav-link" to="/api/v1/post">+ Add new post</Link>
+                        </li>
+                    </ul>
+                    <button className="btn btn-primary ml-auto" onClick={this.handleOnClickSignOut}>Sign out</button>
+                </div>
+            )
+        } else {
+            return (
+                <div className="collapse navbar-collapse">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item active">
+                            <Link className="nav-link" to="/api/v1/user/login">Sign in</Link>
+                        </li>
+                        <li className="nav-item active">
+                            <Link className="nav-link" to="/api/v1/user/register">Sign up</Link>
+                        </li>
+                    </ul>
+                    <button className="btn btn-primary" onClick={this.handleOnClickSignOut}>Sign out</button>
+                </div>
+            )
+        }
     }
 }
 
