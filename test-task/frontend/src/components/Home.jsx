5ce3b2e52e7d8867d5b2b57c13d0a921ff48fdee
Alexey Zaets <3aets.dev@gmail.com>
2019-04-01 17:49:39 +0300

    fix login bug

--- test-task/frontend/src/components/Home.jsx
+++ test-task/frontend/src/components/Home.jsx
@@ -1,7 +1,6 @@
 import React, {Component} from 'react'
 import fetch from 'isomorphic-fetch'
-import {Link, Route} from 'react-router-dom'
-import Post from './Post'
+import {Link} from 'react-router-dom'
 
 
 class Home extends Component {
